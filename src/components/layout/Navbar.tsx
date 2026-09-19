3b536afbdee8133c229b934091a47aebd04c8cb4
anthobldr <anthony.lutard33@gmail.com>
2026-09-19 14:19:02 +0200

    style(navbar): add responsive design for burger menu

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -21,13 +21,13 @@ export default function Navbar() {
                 <nav className="navbar navbar-expand-lg">
                     <div className="container-fluid">
                         <div className="d-flex align-items-center">
-                            <img src="/logo.png" alt="Logo PlayZone" width="150" className="ms-4" />
+                            <img src="/logo.png" alt="Logo PlayZone" width="150" className="ms-lg-4" />
                         </div>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Ouvrir le menu de navigation">
+                        <button className={`navbar-toggler bg-white shadow-sm ${style.menuBurger}`} type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Ouvrir le menu de navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className={`d-flex navbar-nav mx-auto mb-2 mb-lg-0 gap-3 ${style.navList}`}>
+                            <ul className={`navbar-nav mx-auto text-center text-lg-start my-5 my-lg-0 mb-lg-0 gap-3 ${style.navList}`}>
                                 {navLinks.map((link, index) => (
                                     <li className="nav-item" key={link.label}>
                                         <Link className={`nav-link ${index === 0 ? style.active : ""}`} to={link.to}>
@@ -36,7 +36,7 @@ export default function Navbar() {
                                     </li>
                                 ))}
                             </ul>
-                            <div className="d-flex gap-3 me-3 flex-wrap justify-content-center">
+                            <div className="d-flex gap-3 me-lg-3 flex-wrap justify-content-center">
                                 <Link to="/auth/login" className={`btn rounded-5 px-3 py-2 ${style.btnConnexion}`}>
                                     <i className="bi bi-person me-2" aria-hidden="true"></i>Se connecter
                                 </Link>
@@ -53,13 +53,13 @@ export default function Navbar() {
                 <nav className="navbar navbar-expand-lg">
                     <div className="container-fluid">
                         <div className="d-flex align-items-center">
-                            <img src="/logo.png" alt="Logo PlayZone" width="150" className="ms-4" />
+                            <img src="/logo.png" alt="Logo PlayZone" width="150" className="ms-lg-4" />
                         </div>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Ouvrir le menu de navigation">
+                        <button className="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Ouvrir le menu de navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className={`d-flex navbar-nav mx-auto mb-2 mb-lg-0 gap-3 ${style.navList}`}>
+                            <ul className={`navbar-nav mx-auto mb-2 mb-lg-0 gap-3 ${style.navList}`}>
                                 {navLinks.map((link, index) => (
                                     <li className="nav-item" key={link.label}>
                                         <Link className={`nav-link ${index === 0 ? style.active : ""}`} to={link.to}>
