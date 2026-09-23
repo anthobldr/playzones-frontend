cfd73f6670f2df81ef54fdb68db74e93eac8867d
anthobldr <anthony.lutard33@gmail.com>
2026-09-23 15:13:04 +0200

    feat(auth): add logout method

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -71,3 +71,14 @@ export async function getCurrentUser(){
 
     return response.json();
 }
+
+export async function logout(){
+    const response = await fetch(`${API_URL}/auth/logout`, {
+        method: "POST",
+        credentials: "include",
+    });
+
+    if(!response.ok){
+        throw new Error("Erreur lors de la déconnexion.");
+    }
+}
